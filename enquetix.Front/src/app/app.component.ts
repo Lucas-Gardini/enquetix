b2f89d8154dc11314a752a3a49831af22a91f7df
Lucas-Gardini <lucasgardini.dias@hotmail.com>
2025-07-20 18:23:19 -0300

    feat: implementação da página inicial e ajustes de configuração
    - feat: adiciona componente Index com template e estilos
    - style: altera configuração do editor para usar aspas duplas
    - refact: ajusta estrutura do app.component e index.routes
    - style: atualiza index.html para incluir meta tags e título corretos
    - test: adiciona testes para o componente Index

--- enquetix.Front/src/app/app.component.ts
+++ enquetix.Front/src/app/app.component.ts
@@ -1,14 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-import { ButtonModule } from 'primeng/button';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  selector: "app-root",
+  imports: [RouterOutlet],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'enquetix.Front';
+  title = "Enquetix";
 }
